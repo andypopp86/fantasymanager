32c93a994510a3fb9b26527c94a93e3da1aaa1a3
andrewpopp86 <66921961+andrewpopp86@users.noreply.github.com>
2023-08-06 15:59:19 -0400

    Fixing color maps

--- draft/views.py
+++ draft/views.py
@@ -12,6 +12,7 @@ from django.db.models.functions import RowNumber, Coalesce
 
 
 from draft import models as d 
+from draft.utils import GENERIC_RANK_COLORS, WEATHER_RANK_COLORS
 
 def get_draft_board_data(request, draft_id):
     player_pool = d.Player.objects.filter(drafted_players__draft__id=draft_id)
@@ -145,14 +146,10 @@ def draft_board(request, draft_id):
     # add made picks to the managers draft dict
     wopacity_map = {1: 1, 2: 0.5, 3: 0.5, 4: 0.6, 5: 0.8}
     for pick in picks_by_adp:
-        wscore = wopacity_map.get(pick.player.team.playoff_weather_score, 1) if pick.player.team else 0
-        pick.weather_opacity = wscore
-        pick.weather_color = f'rgb(0, 255, 0, {wscore})' if wscore <= 2 else f'rgb(0, 0, 255, {wscore})' if wscore <= 4 else f'rgb(255, 0, 0, {wscore})'
-        sched_score = (pick.player.team.early_season_schedule or 0) / 32 if pick.player.team else 0
+        wscore = pick.player.team.playoff_weather_score if pick.player.team else 0
+        pick.weather_color = WEATHER_RANK_COLORS.get(wscore, WEATHER_RANK_COLORS[100])
         sched_rank = pick.player.team.early_season_schedule if pick.player.team else 0
-        pick.schedule_color = 'green' if sched_rank <= 10 else 'blue' if sched_rank <= 20 else 'red'
-        pick.schedule_opacity = 1 - sched_score if sched_rank <= 16 else sched_score
-        pick.schedule_color = f'rgb(0, 255, 0, {pick.schedule_opacity})' if sched_rank <= 10 else f'rgb(0, 0, 255, {pick.schedule_opacity})' if sched_rank <= 20 else f'rgb(255, 0, 0, {pick.schedule_opacity})'
+        pick.schedule_color = GENERIC_RANK_COLORS.get(sched_rank)
         if pick.manager:
             draft_dict[pick.manager.id].append(pick)
     
