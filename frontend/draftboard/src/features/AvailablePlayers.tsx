f779fcecc008c597b1b1759c6359a02819aa1445
andrewpopp86 <66921961+andrewpopp86@users.noreply.github.com>
2024-07-13 20:32:46 -0400

    add filter to available players

--- frontend/draftboard/src/features/AvailablePlayers.tsx
+++ frontend/draftboard/src/features/AvailablePlayers.tsx
@@ -12,9 +12,84 @@ export const AvailablePlayers = ({draftContext, draftSend}) => {
             player: draftContext.undraftedPlayers.find((player) => player.player.id === id),
         });
     }
+    const [nameFilterValue, setNameFilterValue] = useState("");
+    const [positionFilterValue, setPositionFilterValue] = useState("");
+    const [priceFilterValue, setPriceFilterValue] = useState(undefined);
+    const [filteredPlayers, setFilteredPlayers] = useState(draftContext.undraftedPlayers);
+
+    const checkName = (player) => {
+        return player.player.name.toLowerCase().includes(nameFilterValue.toLowerCase());
+    }
+    const checkPosition = (player) => {
+        return player.player.position.toLowerCase().includes(positionFilterValue.toLowerCase());
+    }
+    const checkPrice = (player) => {
+        return player.player.projected_price <= priceFilterValue;
+    }
+    const handleFilterChange = () => {
+        const predicates = [];
+        if (nameFilterValue !== "") { predicates.push(checkName); }
+        if (positionFilterValue !== "") { predicates.push(checkPosition); }
+        if (priceFilterValue !== 0) { predicates.push(checkPrice); }
+        if (predicates.length === 0) {
+            setFilteredPlayers(draftContext.undraftedPlayers);
+            return;
+        }
+        const filtered = draftContext.undraftedPlayers.filter((player) => {
+            return predicates.every((predicate) => predicate(player));
+        });
+        setFilteredPlayers(filtered);
+    }
+
+    const handleNameFilterChange = (filterValue) => {
+        setNameFilterValue(filterValue.trim());
+    }
+    const handlePositionFilterChange = (filterValue) => {
+        const cleanedValue = filterValue.trim().toUpperCase();
+        if (["QB", "RB", "WR", "TE", "K", "DEF"].includes(cleanedValue)) {
+            setPositionFilterValue(cleanedValue);
+        } else {
+            setPositionFilterValue("");
+        }
+    }
+    const handlePriceFilterChange = (filterValue) => {
+        const floatValue = parseFloat(filterValue);
+        setPriceFilterValue(floatValue || 0);
+    }
+    const clearFilter = () => {
+        setNameFilterValue("");
+        setPositionFilterValue("");
+        setPriceFilterValue(0.00);
+        setFilteredPlayers(draftContext.undraftedPlayers);
+    }
+
     return (
         <div>
             <div style={{fontSize: "24px", fontWeight: "bold"}}>Available Players</div>
+            <table className="table">
+                <tbody>
+                    <tr>
+                        <td scope="row">Name:</td>
+                        <td><input type="text" style={{width: "100px"}}
+                            onBlur={(e) => handleNameFilterChange(e.target.value)} />
+                        </td>
+                        <td><button className={"px-1 py-0 bg-blue-500 text-white rounded-md shadow-md hover:bg-blue-600"} onClick={handleFilterChange}>Filter</button></td>
+                    </tr>
+                    <tr>
+                        <td scope="row">Position:</td>
+                        <td><input type="text" style={{width: "100px"}}
+                            onBlur={(e) => handlePositionFilterChange(e.target.value)} />
+                        </td>
+                        <td><button className={"px-1 py-0 bg-gray-300 text-gray-800 rounded-md shadow-md hover:bg-gray-400"} onClick={clearFilter}>Clear</button></td>
+                    </tr>
+                    <tr>
+                        <td scope="row">Price:</td>
+                        <td><input type="number" style={{width: "100px"}}
+                        className="py-1 px-1 text-sm border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+                        onBlur={(e) => handlePriceFilterChange(e.target.value)} /></td>
+                    </tr>
+                </tbody>
+            </table>
             <table>
                 <thead>
                     <tr>
@@ -24,7 +99,7 @@ export const AvailablePlayers = ({draftContext, draftSend}) => {
                     </tr>
                 </thead>
                 <tbody>
-                    {draftContext.undraftedPlayers?.map((player) => (
+                    {filteredPlayers?.map((player) => (
                         <AvailablePlayer
                             key={player.player.id}
                             player={player}
